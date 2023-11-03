c59180d795093066993a3f556b1f6bd63ab1b8e2
Shifa <luh.shifa@gmail.com>
2023-11-03 18:22:28 +0800

    Friday, Nov 03 2023

--- smerti-web/js/script.js
+++ smerti-web/js/script.js
@@ -5,3 +5,58 @@ btnNavEl.addEventListener("click", function () {
   // memberikan efek pada menu, saat dipilih, maka akan menampilkan menu list
   headerEl.classList.toggle("nav-open");
 });
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    // jika sebuah link tidak memiliki link, hanya hast maka page akan kembali ke atas
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    //Scoll to other link
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+      // console.log(sectionEl);
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+
+    // console.log(e);
+  });
+});
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+observer.observe(sectionHeroEl);
